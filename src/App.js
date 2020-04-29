56d8c31aef9a924a56f7f482227a75edff104bbb
Tay Shen <stay9@vt.edu>
2020-04-29 17:19:47 -0400

    updating packages, hardcoding some temporary ip's for openshift

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
-
   useEffect(() => {
-    axios.get('/auth').then(res => {
+    axios.get("/auth").then((res) => {
       console.log(res);
-    })
+    });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
-      <Button href="http://localhost:3001/auth">Authenticate</Button>
+
+        <Button href="http://10.131.20.136:8080/auth">Authenticate</Button>
       </header>
     </div>
   );
